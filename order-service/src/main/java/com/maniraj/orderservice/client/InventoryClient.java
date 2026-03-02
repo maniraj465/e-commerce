fa0b4a1caa89e683264d6292a65205df844c1703
Maniraj Sivasubbu <38633966+maniraj465@users.noreply.github.com>
2026-03-02 13:20:00 +0530

    Replaced openfeign library with RestClient

--- order-service/src/main/java/com/maniraj/orderservice/client/InventoryClient.java
+++ order-service/src/main/java/com/maniraj/orderservice/client/InventoryClient.java
@@ -1,13 +1,15 @@
 package com.maniraj.orderservice.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(name = "inventory-service", url = "http://localhost:8082")
+//@FeignClient(name = "inventory-service", url = "http://localhost:8082")
 public interface InventoryClient {
 
-    @GetMapping("/api/inventory")
+//    @GetMapping("/api/inventory")
+    @GetExchange("/api/inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 
 }
